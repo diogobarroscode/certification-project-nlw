0f7b46729ca7d082538a871c26bbbb6047c94321
diogobarroscode <diogobarros.code@gmail.com>
2024-02-15 19:19:34 -0300

    first commit

---
+++ src/test/java/com/diogobarroscode/certification/CertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogobarroscode.certification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
