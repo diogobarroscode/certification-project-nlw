3d4909e1f42ee901cae6c4d106018249a9875ebc
diogobarroscode <diogobarros.code@gmail.com>
2024-02-16 02:02:45 -0300

    Add lombok annotation for entity student

--- src/main/java/com/diogobarroscode/certification/modules/students/entities/StudentEntity.java
+++ src/main/java/com/diogobarroscode/certification/modules/students/entities/StudentEntity.java
@@ -14,7 +14,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity(name = "students")
 public class StudentEntity {
 
