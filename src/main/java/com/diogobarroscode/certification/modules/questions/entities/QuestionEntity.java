a8a6a4a97b43833d85f06f38f379477257268a7b
diogobarroscode <diogobarros.code@gmail.com>
2024-02-26 22:29:30 -0300

    add anotation CreationTimestamp for entity question

--- src/main/java/com/diogobarroscode/certification/modules/questions/entities/QuestionEntity.java
+++ src/main/java/com/diogobarroscode/certification/modules/questions/entities/QuestionEntity.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -35,6 +37,7 @@ public class QuestionEntity {
 	@JoinColumn(name = "question_id")
 	private List<AlternativesEntity> alternatives;
 	
+	@CreationTimestamp
 	private LocalDateTime createdAt;
 	
 }
