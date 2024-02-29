52ed20cda9882725bc3b0da28fbdca82fcc2b772
diogobarroscode <diogobarros.code@gmail.com>
2024-02-28 23:33:22 -0300

    Create repositorie for find student by email

---
+++ src/main/java/com/diogobarroscode/certification/modules/students/repositories/StudentRepository.java
@@ -0,0 +1,14 @@
+package com.diogobarroscode.certification.modules.students.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diogobarroscode.certification.modules.students.entities.StudentEntity;
+
+public interface StudentRepository extends JpaRepository<StudentEntity, UUID> {
+	
+	public Optional<StudentEntity> findByEmail(String email);
+
+}
