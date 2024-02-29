d510cea18f444c9fa59d4b2d579754cf7604f887
diogobarroscode <diogobarros.code@gmail.com>
2024-02-28 23:35:23 -0300

    Create repositorie certification to jpql custom for find certification to student and top 10 grade

---
+++ src/main/java/com/diogobarroscode/certification/modules/students/repositories/CertificationStudentRepository.java
@@ -0,0 +1,22 @@
+package com.diogobarroscode.certification.modules.students.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.diogobarroscode.certification.modules.students.entities.CertificationStudentEntity;
+
+public interface CertificationStudentRepository extends JpaRepository<CertificationStudentRepository, UUID> {
+	
+	@Query("SELECT c FROM certifications c "
+			+ "INNER JOIN c.studentEntity std "
+			+ "WHERE std.email = :email "
+			+ "AND c.technology = :technology")
+	List<CertificationStudentEntity> findByStudentEmailAndTechnology(String email, String technology);
+	
+	@Query("SELECT c FROM certifications c ORDER BY c.grade DESC LIMIT 10")
+	List<CertificationStudentEntity> findTop10ByOrderByGradeDesc();
+	
+}
